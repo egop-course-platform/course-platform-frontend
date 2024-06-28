1b1dae5e290cb8d94e6bab9ca2077098e7c2ff44
bird_egop <sampletext32@bk.ru>
2024-06-29 01:54:51 +0300

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'course-platform-frontend';
+
+  constructor(private _httpClient: HttpClient) {
+  }
+
+  textChanged($event: KeyboardEvent) {
+    this._httpClient.post<string[]>('http://localhost:5200/completion2', {code: ($event.target as any).value })
+      .subscribe(x => console.log(x))
+  }
 }
